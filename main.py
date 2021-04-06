8b65a2d05a71b05f16613cc1f8b1f27b39226fb1
chijeukimbap <72594582+chijeukimbap@users.noreply.github.com>
2021-04-06 22:04:32 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from PIL import ImageGrab
+from datetime import datetime
+import keyboard
+
+print("Screenshot running")
+print("To take screenshot press | key ")
+subject = input("Please enter subject: ")
+topic = input("Please enter topic: ")
+while True:
+    try:
+        
+        if keyboard.is_pressed('|'):
+            image = ImageGrab.grab()
+            dt = datetime.now()
+            fname = "{}_{}_{}.{}.png".format(subject,topic,dt.strftime("%H%M_%S"), dt.microsecond // 100000)
+            image.save(fname, 'png')
+            print(fname + " saved")
+
+
+        else:
+            pass
+    except:
+        break
+        print("You shouldn't be here")
+
+
+
+
+
